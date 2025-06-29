9c93cab76c0084c336daa7fe4c7df02ee3726a41
drmelon <drmelon.mail@gmail.com>
2025-06-29 20:35:25 +0100

    Added LDTK project for Island

--- tiled_proj/desert_bg.tsx
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="desert_bg" tilewidth="16" tileheight="16" tilecount="42" columns="7">
- <transformations hflip="0" vflip="0" rotate="0" preferuntransformed="1"/>
- <image source="desert_bg_tiles.png" width="112" height="96"/>
-</tileset>
